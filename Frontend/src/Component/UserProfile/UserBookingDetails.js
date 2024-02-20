2683d2e61af43ffe933f4987f1af8d302d07f539
Chamodi99 <primalshachamodi99@gmail.com>
2024-02-20 10:13:45 +0530

    axios_profile

--- Frontend/src/Component/UserProfile/UserBookingDetails.js
+++ Frontend/src/Component/UserProfile/UserBookingDetails.js
@@ -6,155 +6,62 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import alluser_css from './Profile.module.css';
+ 
+  const BookingCard = () => {
+      const storedUserDetails = localStorage.getItem('facilityUser');
+      const userType = JSON.parse(storedUserDetails).userDetails.userType ;
 
-const storedUserDetails = localStorage.getItem('facilityUser');
-    const storedUser = JSON.parse(storedUserDetails);
-    const userType = JSON.parse(storedUserDetails).userDetails.userType;
-
-const fetchBookings = async () => {
-  try {
-    const response = await axios.get('http://localhost:4000/api/booking/getAllBookings');
-    return response.data;
-  } catch (error) {
-      console.error('Error fetching bookings:', error);
-      throw error;
-  }
-};
-    
-    const BookingCard = () => {
-      const [bookings, setBookings] = useState('');
+      const [bookings, setBookings] = useState([]);
       const [loading, setLoading] = useState(true);
     
       useEffect(() => {
-        // Fetch booking details when the component mounts
         const fetchData = async () => {
           try {
-            const data = await fetchBookings();
-            setBookings(data);
+            let response;
+            if (userType === 'admin' || userType === 'DVC' || userType === 'Director' ) {
+              response = await axios.get('http://localhost:4000/api/booking/getAllBookings');
+            } else if(userType==='university'){
+              response = await axios.get('http://localhost:4000/api/booking/getBooking/:userID');
+            }
+            console.log("res",response.data);
+            setBookings(response.data);
             setLoading(false);
           } catch (error) {
-            console.error('Error fetching bookings:', error);
+            console.error('Error fetching data:', error);
+            setLoading(false);
           }
         };
     
         fetchData();
-      }, []); // Empty dependency array ensures this effect runs once on mount
+      }, []);
     
 
+
       return (
-        <Box  display="flex" flexWrap="wrap" justifyContent="center">
-          {/* <Card variant="outlined"> */}
-            {loading ? (
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  No Booking Details
-                </Typography>
-              </CardContent>
-            ) : (
-              <>
-              {userType === 'admin' && (
-                <>
-                {bookings.map((booking,index) => (
-                  <Card key={index} style={{ margin: '10px' ,maxWidth:275}}>
-                    <CardContent>
-                      <Typography color="text.secondary" gutterBottom>                        
-                        Booking ID: {booking.id}
-                      </Typography>
-                      <Typography variant="h5" component="div">
-                        Facility: {booking.facility}
-                      </Typography>
-                      <Typography variant="body2">
-                        Organization: {booking.organizationName}
-                      </Typography>
-                      <Typography variant="body2">
-                        Date: {new Date(booking.bookingDate).toLocaleDateString()}
-                      </Typography>
-                      <Typography variant="body2">
-                          Time: {booking.Time}
-                      </Typography>
-                      <Typography variant="body2">
-                          Status: {booking.status}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                ))}
-              </>
-            )}
-
-            {userType === 'Guest' ||  userType === 'university' && (
-              <>
-                  {bookings
-                      .filter(booking => booking.userID === storedUser.userDetails.userID)
-                      .map((booking, index) => (
-                          <Card key={index} style={{ margin: '10px', maxWidth: 275 }}>
-                              <CardContent>
-                                  <Typography color="text.secondary" gutterBottom>
-                                      Booking ID: {booking.id}
-                                  </Typography>
-                                  <Typography variant="h5" component="div">
-                                      Facility: {booking.facility}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Organization: {booking.organizationName}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Date: {new Date(booking.bookingDate).toLocaleDateString()}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Time: {booking.Time}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Status: {booking.status}
-                                  </Typography>
-                              </CardContent>
-                              <CardActions>
-                                  <Button size="small">Learn More</Button>
-                              </CardActions>
-                          </Card>
-                      ))}
-              </>
-            )}
-
-            {userType === 'DVC' || 'Director' && (
-              <>
-                  {bookings
-                      .filter(booking => booking.userID === storedUser.userDetails.userID && booking.status === 'pending')
-                      .map((booking, index) => (
-                          <Card key={index} style={{ margin: '10px', maxWidth: 275 }}>
-                              <CardContent>
-                                  <Typography color="text.secondary" gutterBottom>
-                                      Booking ID: {booking.id}
-                                  </Typography>
-                                  <Typography variant="h5" component="div">
-                                      Facility: {booking.facility}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Organization: {booking.organizationName}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Date: {new Date(booking.bookingDate).toLocaleDateString()}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Time: {booking.Time}
-                                  </Typography>
-                                  <Typography variant="body2">
-                                      Status: {booking.status}
-                                  </Typography>
-                              </CardContent>
-                              <CardActions>
-                                  <Button size="small">Learn More</Button>
-                              </CardActions>
-                          </Card>
-                      ))}
-              </>
-            )}
-          </>
-        )}
-      </Box>
-    );
-  };
-export default BookingCard;
+        <Box display="flex" justifyContent="center" mt={4}>
+      {loading ? (
+        <Typography variant="h6">Loading...</Typography>
+      ) : (
+        bookings.map((booking, index) => (
+          <Card key={index} sx={{ minWidth: 275, margin: '10px' }} variant="outlined">
+            <CardContent>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Organization : {booking.organizationName}<br></br>
+              Facility : {booking.facility}<br></br>
+              Date : {new Date(booking.date).toLocaleDateString()}<br />
+              Time : {booking.Time}<br /><br/>
+              Status : {booking.status}
+              </Typography>
+          
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        ))
+      )}
+    </Box>
+      );
+    };
+    
+    export default BookingCard;
